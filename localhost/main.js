76730326997cbc1b33edff453e6f341605c8c779
WowMuchDoge <97766806+forty-eridani@users.noreply.github.com>
2025-12-22 17:04:23 -0600

    Fixes

--- localhost/main.js
+++ localhost/main.js
@@ -1,27 +1,50 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const fs = require('fs')
+const https = require('https')
+const { Server } = require("http")
 
 const port = 8080
 
 app.use(express.static(path.join(__dirname, '../public',)))
-app.use(express.json())
 
-app.listen(port, function() {
-    console.log(`Listening on port ${port}`)
+const options = {
+    key: fs.readFileSync(path.join(__dirname, '../' + 'certs/server.key'), 'utf8'),
+    cert: fs.readFileSync(path.join(__dirname, '../' + 'certs/server.crt'), 'utf-8')
+}
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`HTTPS on port ${port}`)
 })
+
+app.use(express.json())
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
 app.get("/:path", function(req, res) {
+    if (req.params.path[1] == 'e')
+        res.status(404).send('Resource Not Found')
+    
     p = path.join(__dirname, "../" + req.params.path)
+
     console.log(p)
     res.sendFile(p)
 })
 
 let submissions = []
+
+function slice(arr, min, max) {
+    let final = []
+
+    for (let i = min; i < max; i++) {
+        final.push(arr[i])
+    }
+
+    return final
+}
 
 function merge(l, r) {
     let lIndex = 0;
@@ -30,31 +53,41 @@ function merge(l, r) {
     let final = []
 
     while (lIndex < l.length && rIndex < r.length) {
-        if (l[lIndex].votes > r[rIndex].votes)
+        if (l[lIndex].votes >= r[rIndex].votes)
             final.push(l[lIndex++])
         else
             final.push(r[rIndex++])
     }
 
-    final = final.concat(l.slice(lIndex, l.length))
-    final = final.concat(r.slice(rIndex, r.length))
+    final = final.concat(l.slice(lIndex))
+    final = final.concat(r.slice(rIndex))
 
     return final
 }
 
 function sortSubmissions(submissions) {
-    if (submissions.length <= 1)
-        return submissions
+    // if (submissions.length <= 1)
+    //     return submissions
 
-    let mid = Math.floor(submissions.length / 2)
+    // let mid = Math.floor(submissions.length / 2)
 
-    left = submissions.slice(0, mid)
-    right = submissions.slice(mid)
+    // left = slice(submissions, 0, mid)
+    // right = slice(submissions, mid, submissions.length)
 
-    left = sortSubmissions(left)
-    right = sortSubmissions(right)
+    // left = sortSubmissions(left)
+    // right = sortSubmissions(right)
 
-    submissions = merge(left, right)
+    // return merge(left, right)
+
+    for (let i = 0; i < submissions.length; i++) {
+        for (let j = 0; j < submissions.length - i - 1; j++) {
+            if (submissions[j].votes < submissions[j + 1].votes) {
+                let tmp = submissions[j]
+                submissions[j] = submissions[j + 1]
+                submissions[j + 1] = tmp
+            }
+        }
+    }
 
     return submissions
 }
@@ -92,7 +125,8 @@ app.post('/submit', (req, res) => {
 })
 
 app.post('/submissions', (_, res) => {
-    submissions = sortSubmissions(submissions)
+    let tmp = sortSubmissions(submissions)
+    submissions = tmp
     console.log(submissions)
     res.send(JSON.stringify(submissions))
 })
@@ -106,7 +140,8 @@ app.post("/vote", (req, res) => {
 })
 
 app.post("/reset", (req, res) => {
-    if (req.body.sigma === "no you stinker") {
+    console.log(req.body)
+    if (req.body.sigma != undefined && req.body.sigma != "no you stinker") {
         res.send("QUIT BEING SUCH A STINKER")
         return
     }
